69116b52141b72217fc12fae09f38ecedbb43e5f
“Magd <majdgalyl@gmail.com>
2021-12-12 16:36:34 +0200

    App Resolving API Error - Attempt Fix 01

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-const KEY = 'AIzaSyBNZK_Er9BkBZRqT969HMmtqOV1tMXdsB8';
+const KEY = "AIzaSyBNZK_Er9BkBZRqT969HMmtqOV1tMXdsB8";
 
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3',
+    baseURL: "https://www.googleapis.com/youtube/v3",
     params: {
-        part: 'snippet',
-        type: 'video',
+        part: "snippet",
+        type: "video",
         maxResults: 5,
         key: KEY,
     },
