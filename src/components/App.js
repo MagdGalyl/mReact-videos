fcfcdaafd23dab4be1af821cbbb8e2f48aab8e19
“Magd <majdgalyl@gmail.com>
2021-12-12 16:35:21 +0200

    App Resolving API Error - Attempt Fix 01

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import SearchBar from './SearchBar'
-import youtube from '../api/youtube';
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+
 class App extends React.Component {
 
     // after getting the response from API,
     // we make state and get/update it using state
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
 
     // Callback that will be called anytime user submit search
     // axios REQUESTs is ASYNC funtions
@@ -16,7 +18,7 @@ class App extends React.Component {
         // now we 'MAKE REQUEST TO the youtube API to FETCH data using our axios pre-config API'
         const response = await youtube.get('/search', {
             params: {
-                q : term
+                q : term,
             }
         })
 
@@ -25,16 +27,24 @@ class App extends React.Component {
         // thats the list of videos we got in the results
         // console.log(response.data.items)
 
-        this.setState({videos: response.data.items})
+        this.setState({ videos: response.data.items });
     }
+
+    onVideoSelect = (video) => {
+        console.log("From the App!", video);
+      };
 
     render(){
         return(
-            <div className = "ui container">
+            <div className = "ui container inverted segment primary">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
 
-                {/* // geting length of result videos array we got using state */}
-                I have {this.state.videos.length} videos
+                {/* Linking the Video List Component that will use our response VIDEOS from state to show video list */}
+                <VideoList 
+                onVideoSelect={this.onVideoSelect} 
+                videos = {this.state.videos} 
+                />
+                {/* {this.state.videos.length}  */}
             </div>
         )
     }
